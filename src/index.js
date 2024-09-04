efd31c4a9f33f7d10601e9320c885b33648516c9
KayanIrani <iranikayan5@gmail.com>
2024-09-05 02:43:43 +0530

    This is the basic frontend Backend interaction for parsing for sih kindly check properly before merging

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter , Route,Routes,Link } from 'react-router-dom';
 
+
+import Layout from './pages/Layout.js';
+import App from './pages/Home.js';
+import Contact from './pages/Contact.js';
+import UserTweets from './pages/UserTweets.js';
+import NoPage from './pages/NoPage.js';
+
+export default function MainApp()
+{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element ={<App/>}></Route>
+                    <Route path="/UserTweet" element={<UserTweets />}></Route>
+                    <Route path="/contact" element ={<Contact/>}></Route>
+                    <Route path="*" element = {<NoPage/>}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<MainApp />);
