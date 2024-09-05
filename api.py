5d144dd20232aa9a9fa62dc16a9b50b3f76d30e0
dtDhruv <dhruvthanawala10@gmail.com>
2024-09-05 23:22:52 +0530

    added pdf func

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+import asyncio
 
 from src.config.logger import log
 from src.config.env import settings
-from src.api.twitter import get_twitter_user_details, get_user_tweets
+from src.api.twitter import get_twitter_user_details, get_user_tweets, create_twitter_user_pdf
 
 app = FastAPI()
 
@@ -26,6 +27,17 @@ async def root(username: str):
 async def root(tweet_input: TweetParams):
     user_tweets = await get_user_tweets(tweet_input.username, "user", tweet_input.number_of_tweets)
     return {"usertweets": user_tweets}
+
+@app.post("/api/v1/twitter/generatereport")
+async def root(tweet_input: TweetParams):
+    coros = []
+    coros.append(get_twitter_user_details(tweet_input.username))
+    coros.append(get_user_tweets(tweet_input.username, "user", tweet_input.number_of_tweets))
+    
+    user_details, user_tweets = await asyncio.gather(*coros)
+    print(user_details)
+    pdf_name = create_twitter_user_pdf({"userdetails":user_details}, {"usertweets": user_tweets}, tweet_input.number_of_tweets)
+    return {"usertweets": pdf_name}
 
 def main():
     uvicorn.run(
